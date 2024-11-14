bca3acae75e317fee41ac8016253ee0ed6c341f5
Brendan Shanks <bshanks@codeweavers.com>
2024-11-14 10:27:58 -0800

    Revert "winevulkan: Update to VK spec version 1.3.296."
    This reverts commit fb256d8b950b5498bc4b2252de7b0b7a714229f7.

--- dlls/winevulkan/loader_thunks.h
+++ dlls/winevulkan/loader_thunks.h
@@ -149,7 +149,6 @@ enum unix_call
     unix_vkCmdEndTransformFeedbackEXT,
     unix_vkCmdEndVideoCodingKHR,
     unix_vkCmdExecuteCommands,
-    unix_vkCmdExecuteGeneratedCommandsEXT,
     unix_vkCmdExecuteGeneratedCommandsNV,
     unix_vkCmdFillBuffer,
     unix_vkCmdInsertDebugUtilsLabelEXT,
@@ -160,7 +159,6 @@ enum unix_call
     unix_vkCmdPipelineBarrier,
     unix_vkCmdPipelineBarrier2,
     unix_vkCmdPipelineBarrier2KHR,
-    unix_vkCmdPreprocessGeneratedCommandsEXT,
     unix_vkCmdPreprocessGeneratedCommandsNV,
     unix_vkCmdPushConstants,
     unix_vkCmdPushConstants2KHR,
@@ -203,7 +201,6 @@ enum unix_call
     unix_vkCmdSetDepthBoundsTestEnable,
     unix_vkCmdSetDepthBoundsTestEnableEXT,
     unix_vkCmdSetDepthClampEnableEXT,
-    unix_vkCmdSetDepthClampRangeEXT,
     unix_vkCmdSetDepthClipEnableEXT,
     unix_vkCmdSetDepthClipNegativeOneToOneEXT,
     unix_vkCmdSetDepthCompareOp,
@@ -329,9 +326,7 @@ enum unix_call
     unix_vkCreateGraphicsPipelines,
     unix_vkCreateImage,
     unix_vkCreateImageView,
-    unix_vkCreateIndirectCommandsLayoutEXT,
     unix_vkCreateIndirectCommandsLayoutNV,
-    unix_vkCreateIndirectExecutionSetEXT,
     unix_vkCreateInstance,
     unix_vkCreateMicromapEXT,
     unix_vkCreateOpticalFlowSessionNV,
@@ -383,9 +378,7 @@ enum unix_call
     unix_vkDestroyFramebuffer,
     unix_vkDestroyImage,
     unix_vkDestroyImageView,
-    unix_vkDestroyIndirectCommandsLayoutEXT,
     unix_vkDestroyIndirectCommandsLayoutNV,
-    unix_vkDestroyIndirectExecutionSetEXT,
     unix_vkDestroyInstance,
     unix_vkDestroyMicromapEXT,
     unix_vkDestroyOpticalFlowSessionNV,
@@ -473,7 +466,6 @@ enum unix_call
     unix_vkGetEventStatus,
     unix_vkGetFenceStatus,
     unix_vkGetFramebufferTilePropertiesQCOM,
-    unix_vkGetGeneratedCommandsMemoryRequirementsEXT,
     unix_vkGetGeneratedCommandsMemoryRequirementsNV,
     unix_vkGetImageMemoryRequirements,
     unix_vkGetImageMemoryRequirements2,
@@ -621,8 +613,6 @@ enum unix_call
     unix_vkUpdateDescriptorSetWithTemplate,
     unix_vkUpdateDescriptorSetWithTemplateKHR,
     unix_vkUpdateDescriptorSets,
-    unix_vkUpdateIndirectExecutionSetPipelineEXT,
-    unix_vkUpdateIndirectExecutionSetShaderEXT,
     unix_vkUpdateVideoSessionParametersKHR,
     unix_vkWaitForFences,
     unix_vkWaitForPresentKHR,
@@ -1644,13 +1634,6 @@ struct vkCmdExecuteCommands_params
     const VkCommandBuffer *pCommandBuffers;
 };
 
-struct vkCmdExecuteGeneratedCommandsEXT_params
-{
-    VkCommandBuffer commandBuffer;
-    VkBool32 isPreprocessed;
-    const VkGeneratedCommandsInfoEXT *pGeneratedCommandsInfo;
-};
-
 struct vkCmdExecuteGeneratedCommandsNV_params
 {
     VkCommandBuffer commandBuffer;
@@ -1724,13 +1707,6 @@ struct vkCmdPipelineBarrier2KHR_params
 {
     VkCommandBuffer commandBuffer;
     const VkDependencyInfo *pDependencyInfo;
-};
-
-struct vkCmdPreprocessGeneratedCommandsEXT_params
-{
-    VkCommandBuffer commandBuffer;
-    const VkGeneratedCommandsInfoEXT *pGeneratedCommandsInfo;
-    VkCommandBuffer stateCommandBuffer;
 };
 
 struct vkCmdPreprocessGeneratedCommandsNV_params
@@ -2019,13 +1995,6 @@ struct vkCmdSetDepthClampEnableEXT_params
 {
     VkCommandBuffer commandBuffer;
     VkBool32 depthClampEnable;
-};
-
-struct vkCmdSetDepthClampRangeEXT_params
-{
-    VkCommandBuffer commandBuffer;
-    VkDepthClampModeEXT depthClampMode;
-    const VkDepthClampRangeEXT *pDepthClampRange;
 };
 
 struct vkCmdSetDepthClipEnableEXT_params
@@ -2985,30 +2954,12 @@ struct vkCreateImageView_params
     VkResult result;
 };
 
-struct vkCreateIndirectCommandsLayoutEXT_params
-{
-    VkDevice device;
-    const VkIndirectCommandsLayoutCreateInfoEXT *pCreateInfo;
-    const VkAllocationCallbacks *pAllocator;
-    VkIndirectCommandsLayoutEXT *pIndirectCommandsLayout;
-    VkResult result;
-};
-
 struct vkCreateIndirectCommandsLayoutNV_params
 {
     VkDevice device;
     const VkIndirectCommandsLayoutCreateInfoNV *pCreateInfo;
     const VkAllocationCallbacks *pAllocator;
     VkIndirectCommandsLayoutNV *pIndirectCommandsLayout;
-    VkResult result;
-};
-
-struct vkCreateIndirectExecutionSetEXT_params
-{
-    VkDevice device;
-    const VkIndirectExecutionSetCreateInfoEXT *pCreateInfo;
-    const VkAllocationCallbacks *pAllocator;
-    VkIndirectExecutionSetEXT *pIndirectExecutionSet;
     VkResult result;
 };
 
@@ -3429,24 +3380,10 @@ struct vkDestroyImageView_params
     const VkAllocationCallbacks *pAllocator;
 };
 
-struct vkDestroyIndirectCommandsLayoutEXT_params
-{
-    VkDevice device;
-    VkIndirectCommandsLayoutEXT DECLSPEC_ALIGN(8) indirectCommandsLayout;
-    const VkAllocationCallbacks *pAllocator;
-};
-
 struct vkDestroyIndirectCommandsLayoutNV_params
 {
     VkDevice device;
     VkIndirectCommandsLayoutNV DECLSPEC_ALIGN(8) indirectCommandsLayout;
-    const VkAllocationCallbacks *pAllocator;
-};
-
-struct vkDestroyIndirectExecutionSetEXT_params
-{
-    VkDevice device;
-    VkIndirectExecutionSetEXT DECLSPEC_ALIGN(8) indirectExecutionSet;
     const VkAllocationCallbacks *pAllocator;
 };
 
@@ -4099,13 +4036,6 @@ struct vkGetFramebufferTilePropertiesQCOM_params
     uint32_t *pPropertiesCount;
     VkTilePropertiesQCOM *pProperties;
     VkResult result;
-};
-
-struct vkGetGeneratedCommandsMemoryRequirementsEXT_params
-{
-    VkDevice device;
-    const VkGeneratedCommandsMemoryRequirementsInfoEXT *pInfo;
-    VkMemoryRequirements2 *pMemoryRequirements;
 };
 
 struct vkGetGeneratedCommandsMemoryRequirementsNV_params
@@ -5259,22 +5189,6 @@ struct vkUpdateDescriptorSets_params
     const VkWriteDescriptorSet *pDescriptorWrites;
     uint32_t descriptorCopyCount;
     const VkCopyDescriptorSet *pDescriptorCopies;
-};
-
-struct vkUpdateIndirectExecutionSetPipelineEXT_params
-{
-    VkDevice device;
-    VkIndirectExecutionSetEXT DECLSPEC_ALIGN(8) indirectExecutionSet;
-    uint32_t executionSetWriteCount;
-    const VkWriteIndirectExecutionSetPipelineEXT *pExecutionSetWrites;
-};
-
-struct vkUpdateIndirectExecutionSetShaderEXT_params
-{
-    VkDevice device;
-    VkIndirectExecutionSetEXT DECLSPEC_ALIGN(8) indirectExecutionSet;
-    uint32_t executionSetWriteCount;
-    const VkWriteIndirectExecutionSetShaderEXT *pExecutionSetWrites;
 };
 
 struct vkUpdateVideoSessionParametersKHR_params
